30d1f6b5b0acce3f7ac7aa90bef4548aa069c3b4
Matthew Malecki <mmalecki4@gmail.com>
2020-04-16 20:17:09 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -21,3 +21,21 @@ def second_challenge
 
   
 end
+
+#  epic_tragedy = {
+#   :montague => {
+#      :patriarch => {},
+#      :matriarch => {},
+#      :hero => {},
+#      :hero_friends => {}
+#   }, 
+#   :capulet => {
+#      :patriarch => {:name => "Lord Capulet", :age => "50"},
+#      :matriarch => {:name => "Lady Capulet", :age => "51"},
+#      :heroine => {:name => "Juliet", :age => "15", :status => "alive"},
+#      :heroine_friends => [
+#          {:name => "Steven", :age => "30", :attitude => "confused"}, 
+#          {:name => "Nurse", :age => "44", :attitude => "worried"}
+#      ]
+#   }
+#  }
